89e6cec353e999b73e0416a38c263c277030f38d
ivanp7 <ivanp7@mail.ru>
2025-07-10 01:42:35 +0300

    refactor(plugin/opencl/scripts/program-builder.py): fix bugs and add support for multiple header/source/library directories

--- plugin/opencl/scripts/program-builder.py
+++ plugin/opencl/scripts/program-builder.py
@@ -16,6 +16,34 @@ from archi.file import File
 
 DEFAULT_MAP_ADDRESS = 0x7f0000000000
 
+class DirFileGroupAction(argparse.Action):
+    """Generalized Action for grouping files under directories for a specific pair.
+    """
+    def __init__(self, option_strings, dest, group_id, is_dir, **kwargs):
+        super().__init__(option_strings, dest, **kwargs)
+        self.group_id = group_id    # For example: 'hdr', 'src', 'lib'
+        self.is_dir = is_dir        # True for dir, False for file
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        # Each group gets its own _map and _last_dir
+        map_name = f'{self.group_id}_map'
+        last_dir_name = f'_{self.group_id}_last_dir'
+
+        file_map = getattr(namespace, map_name)
+
+        if not hasattr(namespace, last_dir_name):
+            setattr(namespace, last_dir_name, "")
+            file_map.setdefault("", [])
+
+        if self.is_dir:
+            directory = values
+            file_map.setdefault(directory, [])
+            setattr(namespace, last_dir_name, directory)
+        else:
+            files = values if isinstance(values, list) else [values]
+            last_dir = getattr(namespace, last_dir_name)
+            file_map[last_dir].extend(files)
+
 parser = argparse.ArgumentParser(
         description="Build an OpenCL library program and write the binaries to file system.")
 
@@ -29,65 +57,75 @@ parser.add_argument('--dev', type=int, default=0, metavar="ID", help="OpenCL dev
 parser.add_argument('--cflags', metavar="FLAGS", help="Compiler flags")
 parser.add_argument('--lflags', metavar="FLAGS", help="Linker flags")
 
-parser.add_argument('--hdrdir', default=".", metavar="PATH", help="Directory of header file(s)")
-parser.add_argument('--hdr', nargs='+', default=[], metavar="PATHNAME", help="Header file(s)")
+parser.add_argument('--hdrdir', metavar="PATH", action=DirFileGroupAction,
+                    group_id='hdr', is_dir=True, help="Directory of header file(s)")
+parser.add_argument('--hdr', nargs='+', metavar="PATHNAME", action=DirFileGroupAction,
+                    group_id="hdr", is_dir=False, help="Header file(s)")
 
-parser.add_argument('--srcdir', default=".", metavar="PATH", help="Directory of source file(s)")
-parser.add_argument('--src', nargs='+', default=[], metavar="PATHNAME", help="Source file(s)")
+parser.add_argument('--srcdir', metavar="PATH", action=DirFileGroupAction,
+                    group_id='src', is_dir=True, help="Directory of source file(s)")
+parser.add_argument('--src', nargs='+', metavar="PATHNAME", action=DirFileGroupAction,
+                    group_id='src', is_dir=False, help="Source file(s)")
 
-parser.add_argument('--libdir', default=".", metavar="PATH", help="Directory of library file(s)")
-parser.add_argument('--lib', nargs='+', default=[], metavar="PATHNAME", help="Library file(s)")
+parser.add_argument('--libdir', metavar="PATH", action=DirFileGroupAction,
+                    group_id='lib', is_dir=True, help="Directory of library file(s)")
+parser.add_argument('--lib', nargs='+', metavar="PATHNAME",  action=DirFileGroupAction,
+                    group_id='lib', is_dir=False, help="Library file(s)")
 
-parser.add_argument('--outdir', default=".", metavar="PATH", help="Directory of build output file")
 parser.add_argument('--out', metavar="PATHNAME", required=True, help="Build output file")
 
+parser.set_defaults(hdr_map={}, src_map={}, lib_map={})
 args = parser.parse_args()
 
-print(f"Generated file: {args.file}")
+print(f"Generated file: '{args.file}'")
 print(f"Map address: 0x{args.mapaddr:x}")
 print()
 print(f"OpenCL platform #: {args.plt}")
 print(f"OpenCL device #: {args.dev}")
-print(f"OpenCL compiler flags: {args.cflags if args.cflags else ""}")
-print(f"OpenCL linker flags: {args.lflags if args.lflags else ""}")
+print(f"OpenCL compiler flags: '{args.cflags if args.cflags else ""}'")
+print(f"OpenCL linker flags: '{args.lflags if args.lflags else ""}'")
 print()
-print(f"Header directory: {args.hdrdir}")
-print(f"Headers: {args.hdr}")
+print("Headers:")
+for dirpath, filepaths in args.hdr_map.items():
+    print(f"    '{dirpath}': {filepaths}")
 print()
-print(f"Source directory: {args.srcdir}")
-print(f"Sources: {args.src}")
+print("Sources:")
+for dirpath, filepaths in args.src_map.items():
+    print(f"    '{dirpath}': {filepaths}")
 print()
-print(f"Library directory: {args.libdir}")
-print(f"Libraries: {args.lib}")
+print("Libraries:")
+for dirpath, filepaths in args.lib_map.items():
+    print(f"    '{dirpath}': {filepaths}")
 print()
-print(f"Build output directory: {args.outdir}")
-print(f"Build output: {args.out}")
+print(f"Build output file: '{args.out}'")
 print()
-
-if len(args.hdr) != len(set(args.hdr)):
-    raise RuntimeError("Header file pathnames must be all unique")
-elif len(args.src) != len(set(args.src)):
-    raise RuntimeError("Source file pathnames must be all unique")
-elif len(args.lib) != len(set(args.lib)):
-    raise RuntimeError("Library file pathnames must be all unique")
 
 ###############################################################################
 # Read contents of all input files
 
 content_headers = {}
-for path in args.hdr:
-    with open(Path(args.hdrdir) / path, 'r') as file:
-        content_headers[path] = file.read()
+for dirpath, filepaths in args.hdr_map.items():
+    for filepath in filepaths:
+        with open(Path(dirpath) / filepath, 'r') as file:
+            if filepath in content_headers:
+                raise ValueError("Duplicate header detected")
+
+            content_headers[filepath] = file.read()
 
 content_sources = {}
-for path in args.src:
-    with open(Path(args.srcdir) / path, 'r') as file:
-        content_sources[path] = file.read()
+for dirpath, filepaths in args.src_map.items():
+    for filepath in filepaths:
+        with open(Path(dirpath) / filepath, 'r') as file:
+            if filepath in content_sources:
+                raise ValueError("Duplicate source detected")
+
+            content_sources[filepath] = file.read()
 
 content_libraries = []
-for path in args.lib:
-    with open(Path(args.srcdir) / path, 'rb') as file:
-        content_libraries.append(file.read())
+for dirpath, filepaths in args.lib_map.items():
+    for filepath in filepaths:
+        with open(Path(dirpath) / filepath, 'rb') as file:
+            content_libraries.append(file.read())
 
 ###############################################################################
 # Forming the list of instructions
@@ -126,26 +164,31 @@ with app['plugin.opencl'] as plugin_opencl:
 
         # Prepare libraries the program depends on
         app['params.library'] = Parameters()
-        app['params.library'].device_id = app['context.opencl'].device_id
-        app['params.library'].context = app['context.opencl']
+        with app['params.library'] as params_library:
+            params_library.device_id = app['context.opencl'].device_id
+            params_library.context = app['context.opencl']
 
-        libraries = []
-        for i, binary in enumerate(content_libraries):
-            app['params.library'].binaries = [CValue(binary)]
-            app[f'context.library[{i}]'] = opencl_program_bin_interface(app['params.library'])
-            libraries.append(app[f'context.library[{i}]'])
-        app['array.libraries'] = libraries
+            libraries = []
+            for i, binary in enumerate(content_libraries):
+                app['array.binary'] = [CValue(binary)]
+                with app['array.binary'] as array_binary:
+                    params_library.binaries = array_binary.elements
 
-        for i in range(len(content_libraries)):
-            del app[f'context.library[{i}]']
+                app[f'context.library[{i}]'] = opencl_program_bin_interface(params_library)
+                libraries.append(app[f'context.library[{i}]'])
+            app['array.libraries'] = libraries
+
+            for i in range(len(content_libraries)):
+                del app[f'context.library[{i}]']
 
         # Form the parameter list for program building
-        app['params.program'] = Parameters(app['params.library'], cflags=args.cflags, lflags=args.lflags)
+        app['params.program'] = Parameters(cflags=args.cflags, lflags=args.lflags)
         app['params.program'].libraries = app['array.libraries']
         app['params.program'].sources = app['hashmap.sources']
         app['params.program'].headers = app['hashmap.headers']
+        app['params.program'].device_id = app['context.opencl'].device_id
+        app['params.program'].context = app['context.opencl']
 
-        del app['params.library']
         del app['hashmap.headers']
         del app['hashmap.sources']
         del app['array.libraries']
@@ -156,8 +199,8 @@ with app['plugin.opencl'] as plugin_opencl:
         del app['params.program']
 
         # Form the parameter list for output file opening
-        app['params.open'] = Parameters(pathname=str(Path(args.outdir) / args.out),
-                                        create=TRUE, truncate=TRUE, readable=TRUE, writable=TRUE, mode=c.c_int(0o644))
+        app['params.open'] = Parameters(pathname=args.out, create=TRUE, truncate=TRUE,
+                                        readable=TRUE, writable=TRUE, mode=c.c_int(0o644))
         app['params.open'].size = app['context.program'].binary_size[0]
 
         # Open the output file
