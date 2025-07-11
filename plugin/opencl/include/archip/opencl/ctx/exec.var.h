09447b7265607fef3041aee29d3e7247b1d63e1d
ivanp7 <ivanp7@mail.ru>
2025-07-11 17:56:38 +0300

    chore(plugin/opencl/ctx/exec): add stub files for kernel execution structure interfaces

--- plugin/opencl/include/archip/opencl/ctx/exec.var.h
+++ plugin/opencl/include/archip/opencl/ctx/exec.var.h
@@ -9,11 +9,19 @@
 
 #include "archi/ctx/interface.typ.h"
 
-ARCHI_CONTEXT_INIT_FUNC(archip_context_opencl_kernel_exec_data_init);   ///< OpenCL event initialization function.
-ARCHI_CONTEXT_FINAL_FUNC(archip_context_opencl_kernel_exec_data_final); ///< OpenCL event finalization function.
+// TODO archip_opencl_event_array
+// variable num_events, setting of events is not needed
 
-extern
-const archi_context_interface_t archip_context_opencl_kernel_exec_data_interface; ///< OpenCL event interface.
+// TODO archip_opencl_work
+// constant num_dimensions, local_work_size may be NULL
+
+// TODO archip_opencl_kernel_enqueue_data
+
+// ARCHI_CONTEXT_INIT_FUNC(archip_context_opencl_kernel_exec_data_init);   ///< OpenCL event initialization function.
+// ARCHI_CONTEXT_FINAL_FUNC(archip_context_opencl_kernel_exec_data_final); ///< OpenCL event finalization function.
+
+// extern
+// const archi_context_interface_t archip_context_opencl_kernel_exec_data_interface; ///< OpenCL event interface.
 
 #endif // _ARCHIP_OPENCL_CTX_EXEC_VAR_H_
 
---
+++ plugin/opencl/src/opencl/ctx/exec.var.c
@@ -0,0 +1,9 @@
+/**
+ * @file
+ * @brief Application context interfaces for OpenCL kernel execution.
+ */
+
+#include "archip/opencl/ctx/exec.var.h"
+
+// TODO
+
