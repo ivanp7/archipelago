4c021b024dec8d7ad7836ce414328cbacbaba33c
ivanp7 <ivanp7@mail.ru>
2025-07-17 18:44:11 +0300

    fix(builtin/convert/string_to_number): fix critical bug when the pointer address was incorrect

--- src/builtin/convert/string_to_number.var.c
+++ src/builtin/convert/string_to_number.var.c
@@ -32,29 +32,9 @@
 #include <stdbool.h>
 #include <stdalign.h>
 
-struct archi_context_converter_string_to_number_data {
-    archi_pointer_t number;
-
-    union {
-        unsigned char as_uchar;
-        unsigned short as_ushort;
-        unsigned long as_ulong;
-        unsigned long long as_ulonglong;
-
-        signed char as_schar;
-        signed short as_sshort;
-        signed long as_slong;
-        signed long long as_slonglong;
-
-        float as_float;
-        double as_double;
-        long double as_longdouble;
-    };
-};
-
 ARCHI_CONTEXT_INIT_FUNC(archi_context_converter_string_to_number_init)
 {
-    struct archi_context_converter_string_to_number_data value = {0};
+    archi_pointer_t value = {0};
 
     if ((params == NULL) || (params->next != NULL))
         return ARCHI_STATUS_EKEY;
@@ -63,21 +43,27 @@ ARCHI_CONTEXT_INIT_FUNC(archi_context_converter_string_to_number_init)
 
     errno = 0;
 
+#define INIT_VALUE(type) do {           \
+    value = (archi_pointer_t){          \
+        .ptr = malloc(sizeof(type)),    \
+        .element = {                    \
+            .num_of = 1,                \
+            .size = sizeof(type),       \
+            .alignment = alignof(type), \
+        },                              \
+    };                                  \
+    if (value.ptr == NULL)              \
+        return ARCHI_STATUS_ENOMEMORY;  \
+    *(type*)value.ptr = (type)parsed;   \
+} while (0)
+
     if (strcmp("as_uchar", params->name) == 0)
     {
         unsigned long parsed = strtoul(params->value.ptr, NULL, 0);
         if ((errno == ERANGE) || (parsed > UCHAR_MAX))
             return ARCHI_STATUS_EVALUE;
 
-        value.as_uchar = (unsigned char)parsed;
-        value.number = (archi_pointer_t){
-            .ptr = &value.as_uchar,
-            .element = {
-                .num_of = 1,
-                .size = sizeof(value.as_uchar),
-                .alignment = alignof(unsigned char),
-            },
-        };
+        INIT_VALUE(unsigned char);
     }
     else if (strcmp("as_ushort", params->name) == 0)
     {
@@ -85,45 +71,31 @@ ARCHI_CONTEXT_INIT_FUNC(archi_context_converter_string_to_number_init)
         if ((errno == ERANGE) || (parsed > USHRT_MAX))
             return ARCHI_STATUS_EVALUE;
 
-        value.as_ushort = (unsigned short)parsed;
-        value.number = (archi_pointer_t){
-            .ptr = &value.as_ushort,
-            .element = {
-                .num_of = 1,
-                .size = sizeof(value.as_ushort),
-                .alignment = alignof(unsigned short),
-            },
-        };
+        INIT_VALUE(unsigned short);
+    }
+    else if (strcmp("as_uint", params->name) == 0)
+    {
+        unsigned long parsed = strtoul(params->value.ptr, NULL, 0);
+        if ((errno == ERANGE) || (parsed > UINT_MAX))
+            return ARCHI_STATUS_EVALUE;
+
+        INIT_VALUE(unsigned int);
     }
     else if (strcmp("as_ulong", params->name) == 0)
     {
-        value.as_ulong = strtoul(params->value.ptr, NULL, 0);
+        unsigned long parsed = strtoul(params->value.ptr, NULL, 0);
         if (errno == ERANGE)
             return ARCHI_STATUS_EVALUE;
 
-        value.number = (archi_pointer_t){
-            .ptr = &value.as_ulong,
-            .element = {
-                .num_of = 1,
-                .size = sizeof(value.as_ulong),
-                .alignment = alignof(unsigned long),
-            },
-        };
+        INIT_VALUE(unsigned long);
     }
     else if (strcmp("as_ulonglong", params->name) == 0)
     {
-        value.as_ulonglong = strtoull(params->value.ptr, NULL, 0);
+        unsigned long long parsed = strtoull(params->value.ptr, NULL, 0);
         if (errno == ERANGE)
             return ARCHI_STATUS_EVALUE;
 
-        value.number = (archi_pointer_t){
-            .ptr = &value.as_ulonglong,
-            .element = {
-                .num_of = 1,
-                .size = sizeof(value.as_ulonglong),
-                .alignment = alignof(unsigned long long),
-            },
-        };
+        INIT_VALUE(unsigned long long);
     }
     else if (strcmp("as_schar", params->name) == 0)
     {
@@ -131,15 +103,7 @@ ARCHI_CONTEXT_INIT_FUNC(archi_context_converter_string_to_number_init)
         if ((errno == ERANGE) || (parsed < SCHAR_MIN) || (parsed > SCHAR_MAX))
             return ARCHI_STATUS_EVALUE;
 
-        value.as_schar = (signed char)parsed;
-        value.number = (archi_pointer_t){
-            .ptr = &value.as_schar,
-            .element = {
-                .num_of = 1,
-                .size = sizeof(value.as_schar),
-                .alignment = alignof(signed char),
-            },
-        };
+        INIT_VALUE(signed char);
     }
     else if (strcmp("as_sshort", params->name) == 0)
     {
@@ -147,106 +111,75 @@ ARCHI_CONTEXT_INIT_FUNC(archi_context_converter_string_to_number_init)
         if ((errno == ERANGE) || (parsed < SHRT_MIN) || (parsed > SHRT_MAX))
             return ARCHI_STATUS_EVALUE;
 
-        value.as_sshort = (signed short)parsed;
-        value.number = (archi_pointer_t){
-            .ptr = &value.as_sshort,
-            .element = {
-                .num_of = 1,
-                .size = sizeof(value.as_sshort),
-                .alignment = alignof(signed short),
-            },
-        };
+        INIT_VALUE(signed short);
+    }
+    else if (strcmp("as_sint", params->name) == 0)
+    {
+        signed long parsed = strtol(params->value.ptr, NULL, 0);
+        if ((errno == ERANGE) || (parsed < INT_MIN) || (parsed > INT_MAX))
+            return ARCHI_STATUS_EVALUE;
+
+        INIT_VALUE(signed int);
     }
     else if (strcmp("as_slong", params->name) == 0)
     {
-        value.as_slong = strtol(params->value.ptr, NULL, 0);
+        signed long parsed = strtol(params->value.ptr, NULL, 0);
         if (errno == ERANGE)
             return ARCHI_STATUS_EVALUE;
 
-        value.number = (archi_pointer_t){
-            .ptr = &value.as_slong,
-            .element = {
-                .num_of = 1,
-                .size = sizeof(value.as_slong),
-                .alignment = alignof(signed long),
-            },
-        };
+        INIT_VALUE(signed long);
     }
     else if (strcmp("as_slonglong", params->name) == 0)
     {
-        value.as_slonglong = strtoll(params->value.ptr, NULL, 0);
+        signed long long parsed = strtoll(params->value.ptr, NULL, 0);
         if (errno == ERANGE)
             return ARCHI_STATUS_EVALUE;
 
-        value.number = (archi_pointer_t){
-            .ptr = &value.as_slonglong,
-            .element = {
-                .num_of = 1,
-                .size = sizeof(value.as_slonglong),
-                .alignment = alignof(signed long long),
-            },
-        };
+        INIT_VALUE(signed long long);
     }
     else if (strcmp("as_float", params->name) == 0)
     {
-        value.as_float = strtof(params->value.ptr, NULL);
+        float parsed = strtof(params->value.ptr, NULL);
         if (errno == ERANGE)
             return ARCHI_STATUS_EVALUE;
 
-        value.number = (archi_pointer_t){
-            .ptr = &value.as_float,
-            .element = {
-                .num_of = 1,
-                .size = sizeof(value.as_float),
-                .alignment = alignof(float),
-            },
-        };
+        INIT_VALUE(float);
     }
     else if (strcmp("as_double", params->name) == 0)
     {
-        value.as_double = strtod(params->value.ptr, NULL);
+        double parsed = strtod(params->value.ptr, NULL);
         if (errno == ERANGE)
             return ARCHI_STATUS_EVALUE;
 
-        value.number = (archi_pointer_t){
-            .ptr = &value.as_double,
-            .element = {
-                .num_of = 1,
-                .size = sizeof(value.as_double),
-                .alignment = alignof(double),
-            },
-        };
+        INIT_VALUE(double);
     }
     else if (strcmp("as_longdouble", params->name) == 0)
     {
-        value.as_longdouble = strtold(params->value.ptr, NULL);
+        long double parsed = strtold(params->value.ptr, NULL);
         if (errno == ERANGE)
             return ARCHI_STATUS_EVALUE;
 
-        value.number = (archi_pointer_t){
-            .ptr = &value.as_longdouble,
-            .element = {
-                .num_of = 1,
-                .size = sizeof(value.as_longdouble),
-                .alignment = alignof(long double),
-            },
-        };
+        INIT_VALUE(long double);
     }
     else
         return ARCHI_STATUS_EKEY;
 
-    struct archi_context_converter_string_to_number_data *context_data = malloc(sizeof(*context_data));
+    archi_pointer_t *context_data = malloc(sizeof(*context_data));
     if (context_data == NULL)
+    {
+        free(value.ptr);
         return ARCHI_STATUS_ENOMEMORY;
+    }
 
     *context_data = value;
 
-    *context = (archi_pointer_t*)context_data;
+    *context = context_data;
     return 0;
 }
 
 ARCHI_CONTEXT_FINAL_FUNC(archi_context_converter_string_to_number_final)
 {
+    free(context->ptr);
     free(context);
 }
 
