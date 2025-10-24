538537538ff26ea1b1794dd3707e26475d1526b9
ivanp7 <ivanp7@mail.ru>
2025-10-24 22:03:00 +0300

    refactor(main): merge 'archi_exe/args' into main.c

--- src/main.c
+++ src/main.c
@@ -25,8 +25,6 @@
 
 #include "archi/context/api/interface.fun.h"
 #include "archipelago/base/ref_count.fun.h"
-#include "archi_exe/args.fun.h"
-#include "archi_exe/args.typ.h"
 #include "archi_exe/input.typ.h"
 #include "archi_exe/input.def.h"
 #include "archi_exe/registry.def.h"
@@ -54,11 +52,26 @@
 #include <stdio.h> // for snprintf()
 #include <stdalign.h> // for alignof()
 
+#include <argp.h> // command line arguments parsing
+
 ///////////////////////////////////////////////////////////////////////////////
+
+struct archi_exe_args {
+    // Execution
+    char **input;      ///< Array of pathnames of input memory-mapped initialization files.
+    size_t num_inputs; ///< Number of input initialization files.
+
+    bool dry_run; ///< Whether dry run is done: initialization instructions are logged only, not executed.
+
+    // Logging
+    bool no_logo;  ///< True if the application logo is not displayed.
+    bool no_color; ///< True if color codes are not used when printing log messages.
+    int verbosity_level; ///< Application verbosity level.
+};
 
 static
 struct {
-    archi_exe_args_t args; ///< Command line arguments.
+    struct archi_exe_args args; ///< Command line arguments.
 
     archi_context_t registry;   ///< The context registry.
     archi_context_t exe_handle; ///< The library handle of the executable itself.
@@ -133,6 +146,12 @@ print_signal_watch_set(
 
 ///////////////////////////////////////////////////////////////////////////////
 
+static
+void
+parse_args(int argc, char *argv[]);
+
+///////////////////////////////////////////////////////////////////////////////
+
 int
 main(
         int argc,
@@ -149,24 +168,7 @@ main(
     archi_exe_log_init_stream(stderr);
 
     // Parse command line arguments
-    switch ((int)archi_exe_args_parse(&archi_process.args, argc, argv))
-    {
-        case 0: // success
-            break;
-
-        case ARCHI_STATUS_EVALUE: // incorrect arguments
-            archi_print("Error: incorrect command line arguments.\n");
-            return 2;
-
-        case ARCHI_STATUS_ENOMEMORY: // memory allocation error
-            archi_print("Error: memory allocation failure while parsing command line arguments.\n");
-            return 3;
-
-        case ARCHI_STATUS_EFAILURE:
-        default: // unknown error
-            archi_print("Error: unknown failure while parsing command line arguments.\n");
-            return 1;
-    }
+    parse_args(argc, argv);
 
     // Set logging verbosity level
     archi_exe_log_init_verbosity(archi_process.args.verbosity_level);
@@ -849,6 +851,162 @@ print_signal_watch_set(
     {
         if (signal_watch_set->f_SIGRTMIN[i])
             archi_print(" SIGRTMIN+%zu", i);
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+#define ARCHI_LOG_VERBOSITY_DEFAULT_LOWER ARCHI_LOG_VERBOSITY_NOTICE ///< Lower of two default verbosity levels.
+#define ARCHI_LOG_VERBOSITY_DEFAULT_HIGHER ARCHI_LOG_VERBOSITY_INFO  ///< Higher of two default verbosity levels.
+
+enum {
+    ARGKEY_DRY_RUN = 'n',
+
+    ARGKEY_NO_LOGO = 'L',
+    ARGKEY_NO_COLOR = 'm',
+    ARGKEY_VERBOSITY = 'v',
+
+    ARGKEY_HELP = 0xFF,
+};
+
+#define _STR(x) #x
+#define STRINGIFY(x) _STR(x)
+
+static
+const struct argp_option args_options[] = {
+    {.doc = "Execution:"},
+
+    {.key = ARGKEY_DRY_RUN,     .name = "dry-run",  .group = 1,
+        .doc = "Simulate initialization: instructions are logged, not executed"},
+
+    {.doc = "Output:"},
+
+    {.key = ARGKEY_NO_LOGO,     .name = "no-logo",  .group = 2,
+                                    .doc = "Don't display the logo"},
+    {.key = ARGKEY_NO_COLOR,    .name = "no-color", .group = 3,
+                                    .doc = "Monochrome logging - don't use ANSI escape codes"},
+    {.key = ARGKEY_VERBOSITY,   .name = "verbose",  .arg = "[LEVEL]", .flags = OPTION_ARG_OPTIONAL, .group = 3,
+                                    .doc = "Set logging verbosity level (0-" STRINGIFY(ARCHI_LOG_VERBOSITY_MAX)
+                                           " or quiet/error/warning/notice/info/debug/max).\n"
+                                           "No argument = info. Default = notice"},
+
+    {0}
+};
+
+static
+error_t
+args_parser_func(int key, char *arg, struct argp_state *state)
+{
+    struct archi_exe_args *args = state->input;
+
+    switch (key)
+    {
+        case ARGKEY_DRY_RUN:
+            args->dry_run = true;
+            break;
+
+        case ARGKEY_NO_LOGO:
+            args->no_logo = true;
+            break;
+
+        case ARGKEY_NO_COLOR:
+            args->no_color = true;
+            break;
+
+        case ARGKEY_VERBOSITY:
+            if (arg == NULL)
+                args->verbosity_level = ARCHI_LOG_VERBOSITY_DEFAULT_HIGHER;
+            else if (!strcmp(arg, "quiet"))
+                args->verbosity_level = ARCHI_LOG_VERBOSITY_QUIET;
+            else if (!strcmp(arg, "error"))
+                args->verbosity_level = ARCHI_LOG_VERBOSITY_ERROR;
+            else if (!strcmp(arg, "warning"))
+                args->verbosity_level = ARCHI_LOG_VERBOSITY_WARNING;
+            else if (!strcmp(arg, "notice"))
+                args->verbosity_level = ARCHI_LOG_VERBOSITY_NOTICE;
+            else if (!strcmp(arg, "info"))
+                args->verbosity_level = ARCHI_LOG_VERBOSITY_INFO;
+            else if (!strcmp(arg, "debug"))
+                args->verbosity_level = ARCHI_LOG_VERBOSITY_DEBUG;
+            else if (!strcmp(arg, "max"))
+                args->verbosity_level = ARCHI_LOG_VERBOSITY_MAX;
+            else if (strlen(arg) == 1)
+            {
+                args->verbosity_level = arg[0] - '0';
+                if ((args->verbosity_level < 0) || (args->verbosity_level > ARCHI_LOG_VERBOSITY_MAX))
+                    return EINVAL;
+            }
+            else
+                return EINVAL;
+            break;
+
+        case ARGP_KEY_ARG:
+            /* Let the ARGP_KEY_ARGS case parse it.  */
+            return ARGP_ERR_UNKNOWN;
+
+        case ARGP_KEY_ARGS:
+            args->input      = state->argv + state->next;
+            args->num_inputs = state->argc - state->next;
+            break;
+
+        case ARGP_KEY_NO_ARGS:
+        case ARGP_KEY_END:
+        case ARGP_KEY_INIT:
+        case ARGP_KEY_SUCCESS:
+        case ARGP_KEY_ERROR:
+        case ARGP_KEY_FINI:
+            break;
+
+        default:
+            return ARGP_ERR_UNKNOWN;
+    }
+
+    return 0;
+}
+
+void
+parse_args(int argc, char *argv[])
+{
+    archi_process.args = (struct archi_exe_args){
+        .verbosity_level = ARCHI_LOG_VERBOSITY_DEFAULT_LOWER,
+    };
+
+    // Parse command line arguments
+    struct argp args_parser = {
+        .options = args_options,
+        .parser = args_parser_func,
+        .args_doc = "[PATHNAME...]",
+        .doc = "\n\
+A versatile, modular application using plugins configured by memory-mapped initialization files that define its specific behavior.\n\
+\v\
+\n\
+BUILT-IN CONTEXTS\n (keys in the application context registry):\n\
+\n\
+ \t\"" ARCHI_EXE_REGISTRY_KEY_REGISTRY "\"\t: the context registry itself\n\
+ \t\"" ARCHI_EXE_REGISTRY_KEY_EXECUTABLE "\"\t: the library context of the executable itself\n\
+ \t\"" ARCHI_EXE_REGISTRY_KEY_INPUT_FILE "\"\t: the context of the current input file\n\
+\n\
+ \t\"" ARCHI_EXE_REGISTRY_KEY_SIGNAL "\"\t: the signal management context\n\
+"
+    };
+
+    error_t err = argp_parse(&args_parser, argc, argv, 0, NULL, &archi_process.args);
+    switch (err)
+    {
+        case 0: // success
+            return;
+
+        case EINVAL: // incorrect arguments
+            archi_print("Error: incorrect command line arguments.\n");
+            exit(2);
+
+        case ENOMEM: // memory allocation error
+            archi_print("Error: memory allocation failure while parsing command line arguments.\n");
+            exit(3);
+
+        default: // unknown error
+            archi_print("Error: unknown failure while parsing command line arguments.\n");
+            exit(1);
     }
 }
 
