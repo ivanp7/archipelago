060e441b02ef2e672ae428254e2ab58344712573
ivanp7 <ivanp7@mail.ru>
2026-04-21 19:14:23 +0300

    feat(python/archi/helper/memory.py): add helpers for memory contexts

---
+++ python/archi/helper/memory.py
@@ -0,0 +1,114 @@
+ #############################################################################
+ # Copyright (C) 2023-2026 by Ivan Podmazov                                  #
+ #                                                                           #
+ # This file is part of Archipelago.                                         #
+ #                                                                           #
+ #   Archipelago is free software: you can redistribute it and/or modify it  #
+ #   under the terms of the GNU Lesser General Public License as published   #
+ #   by the Free Software Foundation, either version 3 of the License, or    #
+ #   (at your option) any later version.                                     #
+ #                                                                           #
+ #   Archipelago is distributed in the hope that it will be useful,          #
+ #   but WITHOUT ANY WARRANTY; without even the implied warranty of          #
+ #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           #
+ #   GNU Lesser General Public License for more details.                     #
+ #                                                                           #
+ #   You should have received a copy of the GNU Lesser General Public        #
+ #   License along with Archipelago. If not, see                             #
+ #   <http://www.gnu.org/licenses/>.                                         #
+ #############################################################################
+
+# @file
+# @brief Helpers for memory contexts.
+
+from contextlib import contextmanager
+import ctypes as c
+
+from archi.context import (
+        Registry,
+        PrimitiveDataPointerContext,
+        MemoryContext,
+        MemoryMappingContext,
+        MemoryInterfaceSymbol,
+        )
+
+
+def heap_memory_interface(plugin):
+    """Obtain memory interface for heap memory.
+    """
+    return MemoryInterfaceSymbol.slot(plugin, 'heap')
+
+
+def new_memory_context(registry, key, c_type, interface, /,
+                       alloc_data=None, ext_alignment=None,
+                       map_data=None, contents=None):
+    """Create a new memory context.
+    """
+    if not isinstance(registry, Registry):
+        raise TypeError
+    elif not isinstance(key, str):
+        raise TypeError
+    elif not isinstance(ext_alignment, (type(None), int)):
+        raise TypeError
+
+    if isinstance(c_type, c.Array):
+        length = len(c_type)
+        stride = c.sizeof(c_type._type_)
+        alignment = c.alignment(c_type._type_)
+    else:
+        length = 1
+        stride = c.sizeof(c_type)
+        alignment = c.alignment(c_type)
+
+    executable = registry[Registry.KEY_EXECUTABLE]
+
+    I_MEMORY = MemoryContext.interface(library=executable)
+
+    params = {}
+    if alloc_data is not None:
+        params['alloc_data'] = alloc_data
+
+    memory_context = registry.new_context(I_MEMORY(interface=interface, length=length,
+                                                   stride=stride, alignment=alignment,
+                                                   **params),
+                                   key=key)
+
+    if contents is not None:
+        I_PDPOINTER = PrimitiveDataPointerContext.interface(library=executable)
+
+        with memory_mapping(registry, memory_context, map_data=map_data) as mapping_context, \
+                registry.temp_context(I_PDPOINTER(pointee=mapping_context.ptr, writable=True),
+                                      key=registry.temp_key(prefix='memory_mapping_ptr')) as mapping_ptr_context:
+            # Initialize the memory
+            registry(mapping_ptr_context.copy(src=contents))
+
+    return memory_context
+
+
+@contextmanager
+def memory_mapping(registry, memory, /, map_data=None, offset=None, length=None):
+    """Context manager for a memory mapping.
+    """
+    if not isinstance(registry, Registry):
+        raise TypeError
+    elif not isinstance(memory, MemoryContext):
+        raise TypeError
+    elif not isinstance(offset, (type(None), int)):
+        raise TypeError
+    elif not isinstance(length, (type(None), int)):
+        raise TypeError
+
+    I_MEMORY_MAPPING = MemoryMappingContext.interface(library=executable)
+
+    params = {}
+    if map_data is not None:
+        params['map_data'] = map_data
+    if offset is not None:
+        params['offset'] = offset
+    if length is not None:
+        params['length'] = length
+
+    with registry.temp_context(I_MEMORY_MAPPING(memory=memory, **params),
+                               key=registry.temp_key(prefix='memory_mapping')) as memory_mapping:
+        yield memory_mapping
+
